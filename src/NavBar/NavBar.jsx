badb257df83a783f3d7dcba62b3336e8ea7f070e
hunhunseunghun <hunhunseunghun@gmail.com>
2021-06-21 13:45:14 +0900

    start

---
+++ src/NavBar/NavBar.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { NavContainer } from "./NavBarStyled";
+import logo from "../Img/logo_w.svg";
+import profileImg from "../Img/profileImg.svg";
+
+const NavBar = () => {
+  return (
+    <NavContainer className="header white">
+      <div className="header_tl">
+        <a href="/">
+          <div className="logo_section">
+            <img src={logo} alt="" className="logoImg" />
+          </div>
+        </a>
+      </div>
+      <div className="main_menu">
+        <ul>
+          <li className="active">
+            <a href="/">메인으로</a>
+          </li>
+          <li className="">
+            <a href="/tutorial">영상제작팁</a>
+          </li>
+          <li className="">
+            <a href="/price">요금제</a>
+          </li>
+          <li>
+            <a href="/login">영상마켓</a>
+          </li>
+          <li>
+            <a className="profileImg" href="/login">
+              <img src={profileImg} alt="" className="profileImg" />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </NavContainer>
+  );
+};
+
+export default NavBar;
---
+++ src/NavBar/NavBarStyled.jsx
@@ -0,0 +1,86 @@
+import styled from "styled-components";
+
+export const NavContainer = styled.div`
+  position: fixed;
+  z-index: 100;
+  width: 100%;
+  padding: 10px 60px 0;
+  height: 90px;
+  display: flex;
+  align-items: center;
+  transition-duration: 0.2s;
+  background-color: gray;
+  box-sizing: border-box;
+
+  textarea {
+    box-sizing: border-box;
+  }
+  .header_tl {
+    display: flex;
+    align-items: center;
+  }
+  a:hover,
+  a:link,
+  a:visited {
+    color: inherit;
+    text-decoration: none;
+    cursor: pointer;
+  }
+  .logo_section {
+    cursor: pointer;
+  }
+
+  .main_menu {
+    flex: 1 1;
+    display: flex;
+    justify-content: flex-end;
+
+    /* li,
+    ul {
+      list-style: none;
+      margin: 0;
+      padding: 0;
+    } */
+
+    ul {
+      display: flex;
+      align-items: center;
+      margin-block-start: 1em;
+      margin-block-end: 1em;
+      margin-inline-start: 0px;
+      margin-inline-end: 0px;
+      padding-inline-start: 40px;
+      margin: 0;
+      padding: 0;
+    }
+
+    li {
+      display: inline-block;
+      cursor: pointer;
+      margin-left: 40px;
+      font-weight: 700;
+      font-size: 16px;
+      position: relative;
+      line-height: 1;
+      text-align: -webkit-match-parent;
+
+      @media only screen and (max-width: 1350px) and (min-width: 900px) {
+        margin-left: 20px;
+      }
+    }
+
+    li.active span:after {
+      content: "";
+      position: absolute;
+      width: 4px;
+      height: 4px;
+      right: -6px;
+      top: 0;
+      border-radius: 2px;
+      background-color: #181818;
+    }
+  }
+  .profileImg {
+    width: 40px;
+  }
+`;
