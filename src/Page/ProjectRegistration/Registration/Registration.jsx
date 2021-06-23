28ad57ddfa26f896347e12f625984141f6b5f711
hunhunseunghun <hunhunseunghun@gmail.com>
2021-06-23 18:17:20 +0900

    06231817

--- src/Page/ProjectRegistration/Registration/Registration.jsx
+++ src/Page/ProjectRegistration/Registration/Registration.jsx
@@ -1,7 +1,92 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { RegiContainer } from "./RegistrationStyled.jsx";
 
 const Registration = () => {
+  const selectDom = useRef(0);
+  const [upLoadFile, setUpLoadFile] = useState(null); //업로드 파일 객체
+  const [uploadFilePath, setUploadFilePath] = useState("Choose file to upload"); //업로드 파일패스 , placholder 값
+  const [editFilePath, setEditFilePath] = useState("Choose file to upload"); //편집 대상 파일 패스, placeholder 값
+  const [editFile, setEditFile] = useState(null);
+  // const [upLoadFileArr, setUploadFileArr] = useState([]); //업로드 파일 배열
+  const fileChangeHandler = (e) => {
+    let fileNames = [];
+    for (let key in e.target.files) {
+      fileNames[key] = e.target.files[key];
+    }
+    // upLoadFileArr.push(e.target.files[0]);
+    setUpLoadFile(e.target.files);
+    console.log("fileList", e.target.files);
+    console.log(upLoadFile);
+
+    //input placeholder handler
+    if (document.getElementById("fileUpLoader").value) {
+      setUploadFilePath(document.getElementById("fileUpLoader").value);
+    } else {
+      setUploadFilePath("Choose file to upload");
+    }
+    // handleFileNames();
+  };
+
+  const editFileChangeHandler = (e) => {
+    setEditFile(e.target.files);
+    console.log(editFile);
+    //input placeholder handler
+    if (document.getElementById("editFileUpLoader").value) {
+      setEditFilePath(document.getElementById("editFileUpLoader").value);
+    } else {
+      setEditFilePath("Choose file to upload");
+    }
+    // handleFileNames();
+  };
+
+  // const handleDeleteFile = (thisIdx) => {
+  //   upLoadFileArr.splice(thisIdx, 1);
+  //   selectDom.current.classList.value = "noDisplay";
+  //   console.log("currDom", selectDom.current);
+  //   console.log(selectDom);
+  //   console.log("delete excute : ", thisIdx);
+  //   console.log(upLoadFileArr);
+  // };
+
+  // const handleFileNames = () => {
+  //   console.log("excute handlefileNames");
+  //   if (upLoadFileArr.length !== 0) {
+  //     return upLoadFileArr.map((ele, idx) => {
+  //       console.log(idx);
+  //       let thisIdx = idx;
+  //       return (
+  //         <div
+  //           className={`inputFileNames${thisIdx} file${thisIdx}`}
+  //           ref={selectDom}
+  //         >
+  //           {ele.name}
+  //           <button onClick={() => handleDeleteFile(thisIdx)} key={thisIdx}>
+  //             x
+  //           </button>
+  //         </div>
+  //       );
+  //     });
+  //   } else {
+  //     return null;
+  //   }
+  // };
+
+  // 첨부 파일 업로드 로직
+  // const uploadFile = async (e) => {
+  //   const formData = new FormData();
+  //   formData.append("file", uploadFile);
+  //   formData.append("fileName", upLoadFileName);
+  //   try {
+  //     const res = await axios.post(
+  //       "http://locathisIdxlhost:3000/upload",
+  //       formData
+  //     );
+  //     console.log(res);
+  //   } catch (ex) {
+  //     console.log(ex);
+  //   }
+  // };
+
   return (
     <RegiContainer>
       <section className="ele request">
@@ -45,6 +130,23 @@ const Registration = () => {
         <div className="menu"> 파일 업로드</div>
         <div className="inputPrjUpload">
           <div>프로젝트 관련한 자료를 업로드 해주세요</div>
+          <div className="uploadTextArea"></div>
+          <input
+            type="text"
+            className="fileRoute"
+            placeholder={uploadFilePath}
+            readOnly
+          />
+          <input
+            type="file"
+            id="fileUpLoader"
+            onChange={fileChangeHandler}
+            multiple
+            className="prjUpload"
+          />
+          {/* <div className="uploadFileArea">
+            {upLoadFileArr !== 0 ? handleFileNames() : null}
+          </div> */}
         </div>
       </section>
       <section className="ele">
@@ -52,6 +154,26 @@ const Registration = () => {
       </section>
       <section className="ele">
         <div className="menu">편집 대상 파일</div>
+        <div className="inputPrjUpload">
+          <div>프로젝트 관련한 자료를 업로드 해주세요</div>
+          <div className="uploadTextArea">
+            <input type="checkbox" />
+            추후 별도 제출 하겠습니다.{" "}
+          </div>
+          <input
+            type="text"
+            className="fileRoute"
+            placeholder={editFilePath}
+            readOnly
+          />
+          <input
+            type="file"
+            id="editFileUpLoader"
+            onChange={editFileChangeHandler}
+            multiple
+            className="editFileUpLoader"
+          />
+        </div>
       </section>
     </RegiContainer>
   );
