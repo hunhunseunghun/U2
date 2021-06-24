8d80fc3223c22e0c76dc4cf8e1ac99b3b78d5931
leehojun0314 <dlghwns0314@naver.com>
2021-06-24 19:37:44 +0900

    'workdetailboxing'

--- src/Page/MyWork/tabs/quester/Quester.jsx
+++ src/Page/MyWork/tabs/quester/Quester.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import QuestTable from '../Subtabs/QuestTable';
+import RegiQuests from '../Subtabs/RegiQuests';
 import datas from './sampledata';
 function Quester() {
 	let [currentTab, setCurrentTab] = useState(0);
@@ -9,7 +9,7 @@ function Quester() {
 	};
 
 	let tabs = {
-		0: <QuestTable datas={datas}></QuestTable>,
+		0: <RegiQuests datas={datas}></RegiQuests>,
 	};
 	return (
 		<div>
---
+++ src/Page/WorkDetail/WorkDetailStyled.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const WorkDetailContainer = styled.div`
+	padding-top: 90px;
+	main {
+		display: flex;
+		text-align: 'center';
+	}
+	article {
+		width: '500px';
+	}
+`;
