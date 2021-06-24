39706e723b059642f121cbb30ab4ab41b37aadc9
leehojun0314 <dlghwns0314@naver.com>
2021-06-25 00:59:15 +0900

    'projectDetailing'

--- src/Page/WorkDetail/WorkDetail.jsx
+++ src/Page/WorkDetail/WorkDetail.jsx
@@ -1,15 +1,25 @@
+import { useState } from 'react';
 import { WorkDetailContainer } from './WorkDetailStyled';
 function WorkDetail(props) {
+	let [subject, setSubject] = useState();
 	return (
 		<WorkDetailContainer>
-			<main style={{ justifyContent: 'center', width: '100%' }}>
+			<section>
+				<section className="section1">
+					<img src="http://ddragon.leagueoflegends.com/cdn/11.13.1/img/champion/Aatrox.png"></img>
+					<div className="project-name">project name</div>
+					<div className="project-info">
+						<div className="project-info spec">대상</div>
+						<div>{subject}</div>
+						<div className="project-info spec">프로젝트 미팅</div>
+						<div className="project-info spec">과제 완료 조건</div>
+						<div className="project-info spec">보상</div>
+					</div>
+				</section>
 				<article style={{ border: 'solid', borderColor: 'black' }}>
-					user info
+					project details
 				</article>
-				<article style={{ border: 'solid', borderColor: 'black' }}>
-					user details
-				</article>
-			</main>
+			</section>
 		</WorkDetailContainer>
 	);
 }
