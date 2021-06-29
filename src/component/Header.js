94713e286fd2e5b7f63c0b651bfc81d01799eb02
hunhunseunghun <hunhunseunghun@gmail.com>
2021-06-30 01:02:32 +0900

    doing

--- src/component/Header.js
+++ src/component/Header.js
@@ -14,6 +14,7 @@ function Header(props) {
   const [windowAppYn, setWindowAppYn] = useState(false);
   const [headerYn, setHeaderYn] = useState(true);
   const [profilePopYn, setProfilePopYn] = useState(false);
+
   const location = useLocation();
   const dispatch = useDispatch();
   const userInfo = useSelector(state => state.userInfo);
@@ -151,8 +152,20 @@ function Header(props) {
           >
             <Link to={'/price'}>요금제</Link>
           </li>
-          <li>
-            <Link to="/creatormarket">영상 마켓</Link>
+          <li
+            className={
+              location.pathname.indexOf('/creatormarket') >= 0
+                ? 'active tab_vidmarket'
+                : 'tab_vidmarket'
+            }
+          >
+            <Link to={'/creatormarket'}>크리에이터 마켓</Link>
+            <div className="vidmarket_dorpdown">
+              <Link to={'/prjregi'} className="dropdown_prjapply">
+                프로젝트 등록
+              </Link>
+              <div className="dropdown_currprj">진행중인 프로젝트</div>
+            </div>
           </li>
           {!userInfo.email && (
             <li>
