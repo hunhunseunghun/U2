d03df6b3662f3cb9f3234426ff1ec123ebe3f53b
leehojun0314 <dlghwns0314@naver.com>
2021-07-01 22:15:15 +0900

    suvmain 기능 의뢰버튼, 남은 일자, 라우팅

---
+++ src/library/timeSetting.js
@@ -0,0 +1,56 @@
+// yyyyMMdd 날짜 문자열을 Date형으로 반환
+export function to_date(date_str) {
+	var yyyyMMdd = String(date_str);
+	var sYear = yyyyMMdd.substring(0, 4);
+	var sMonth = yyyyMMdd.substring(4, 6);
+	var sDate = yyyyMMdd.substring(6, 8);
+
+	return new Date(Number(sYear), Number(sMonth) - 1, Number(sDate));
+}
+
+//yyyy-MM-dd 날짜 문자열을 Date형으로 반환
+export function to_date2(date_str) {
+	var yyyyMMdd = String(date_str);
+	var sYear = yyyyMMdd.substring(0, 4);
+	var sMonth = yyyyMMdd.substring(5, 7);
+	var sDate = yyyyMMdd.substring(8, 10);
+
+	//alert("sYear :"+sYear +"   sMonth :"+sMonth + "   sDate :"+sDate);
+	return new Date(Number(sYear), Number(sMonth) - 1, Number(sDate));
+}
+
+//Date형을 구분자로 구분된 형식의 날짜 문자열 변환
+export function get_date_str_gubun(date, gubun) {
+	var sYear = date.getFullYear();
+	var sMonth = date.getMonth() + 1;
+	var sDate = date.getDate();
+
+	sMonth = sMonth > 9 ? sMonth : '0' + sMonth;
+	sDate = sDate > 9 ? sDate : '0' + sDate;
+	return sYear + gubun + sMonth + gubun + sDate;
+}
+
+//일 차이 : date2(일) - date1(일)
+//date1 === '2021-07-01
+export function between_date(date1, date2) {
+	var y1970 = new Date(1970, 0, 1).getTime();
+	var time1 = null;
+	var time2 = null;
+
+	if (date1.length > 8) time1 = to_date2(date1).getTime() - y1970;
+	else time1 = to_date(date1).getTime() - y1970;
+
+	if (date2.length > 8) time2 = to_date2(date2).getTime() - y1970;
+	else time2 = to_date(date2).getTime() - y1970;
+
+	var per_day = 1000 * 60 * 60 * 24; // 1일 밀리초
+
+	return Math.floor(time1 / per_day) - Math.floor(time2 / per_day);
+}
+
+//dateFin === '2021-07-15T14:59:59+00:00'
+export function calcRemainDays(dateFin) {
+	var newDateFin = dateFin.split('T')[0];
+	var newDateNow = get_date_str_gubun(new Date(), '-');
+	return between_date(newDateFin, newDateNow);
+}
--- src/page/CreatorMarket/ContentElement/ContentElement.jsx
+++ src/page/CreatorMarket/ContentElement/ContentElement.jsx
@@ -1,74 +1,81 @@
 import React, { useState, useEffect } from 'react';
+import { FiCheck, FiX } from 'react-icons/fi';
 import { EleContainer } from './ContentElementStyled.jsx';
 import { LinearProgress } from '@material-ui/core';
 import ceImg from '../../../Img/ceImg.png';
+import { calcRemainDays } from '../../../library/timeSetting.js';
 
 const ContentElement = ({ challenge }) => {
-  return (
-    <EleContainer>
-      <div className="challenge_img_area">
-        <img src={ceImg} alt={ceImg} className="challenge_img" />
-        {/* {tItem.bannerImage === null && (
+	console.log('challnege: ', challenge);
+
+	return (
+		<EleContainer>
+			<div className="challenge_img_area">
+				<img src={ceImg} alt={ceImg} className="challenge_img" />
+				{/* {tItem.bannerImage === null && (
           <div className={'no_img'}>
             <img src={'/img/no_image.png'} />
             <span className={'not_contents'}>Image not found</span>
           </div>
         )} */}
-      </div>
-      <div className="challenge_contents">
-        <h1 className="challenge_title">{challenge.title}</h1>
-        <div className="challenge_sub">{challenge.subtitle}</div>
-      </div>
-      <div className="challenge_bottom">
-        <div className="challenge_progress_area">
-          <div className="challenge_progress_text">
-            {/* <button className="paticipantBtn">+</button> */}
-            <div>참가자</div> <div>{`${challenge.challengerCount}`}</div>
-          </div>
-          <LinearProgress
-            className="challenge_progressBar"
-            variant="determinate"
-            color="secondary"
-            value={challenge.challengerCount}
-          />
-        </div>
-        <section className="challenge_info_top">
-          <div className="meetArea">
-            <input type="checkbox" name="chk_info" value="HTML" />
-            <div className="meetText">OFF 미팅</div>
-          </div>
-          <div className="budgetArea">
-            <div className="budget">
-              {challenge.reward !== null ? challenge.reward : '--'}
-            </div>
-            <div className="bugetText">예산금액</div>
-          </div>
-          <div className="remainDateArea">
-            <div className="remainDate">00일</div>
-            <div className="remainText">남은 일자</div>
-          </div>
-        </section>
+			</div>
+			<div className="challenge_contents">
+				<h1 className="challenge_title">{challenge.title}</h1>
+				<div className="challenge_sub">{challenge.subtitle}</div>
+			</div>
+			<div className="challenge_bottom">
+				<div className="challenge_progress_area">
+					<div className="challenge_progress_text">
+						{/* <button className="paticipantBtn">+</button> */}
+						<div>참가자</div> <div>{`${challenge.challengerCount}`}</div>
+					</div>
+					<LinearProgress
+						className="challenge_progressBar"
+						variant="determinate"
+						color="secondary"
+						value={challenge.challengerCount}
+					/>
+				</div>
+				<section className="challenge_info_top">
+					<div className="meetArea">
+						{/* <input type="checkbox" name="chk_info" value="HTML" /> */}
+						{challenge.meetCode === 0 ? <FiX /> : <FiCheck />}
+						<div className="meetText">OFF 미팅</div>
+					</div>
+					<div className="budgetArea">
+						<div className="budget">
+							{challenge.reward !== null ? challenge.reward : '--'}
+						</div>
+						<div className="bugetText">예산금액</div>
+					</div>
+					<div className="remainDateArea">
+						<div className="remainDate">
+							{calcRemainDays(challenge.missions[0].dateFin)}일
+						</div>
+						<div className="remainText">남은 일자</div>
+					</div>
+				</section>
 
-        <section className="challenge_info_bot">
-          <div className="challenge_cmtval_area">
-            <img
-              src={require('../../../Img/comment.svg').default}
-              className="cmntIcon"
-            />
-            <div className="cmntVal">{challenge.commentCount}</div>
-          </div>
+				<section className="challenge_info_bot">
+					<div className="challenge_cmtval_area">
+						<img
+							src={require('../../../Img/comment.svg').default}
+							className="cmntIcon"
+						/>
+						<div className="cmntVal">{challenge.commentCount}</div>
+					</div>
 
-          <div className="challenge_shareval_area">
-            <img
-              src={require('../../../Img/share.svg').default}
-              className="shareIcon"
-            />
-            <div className="shareVal">{challenge.shareCount}</div>
-          </div>
-        </section>
-      </div>
-    </EleContainer>
-  );
+					<div className="challenge_shareval_area">
+						<img
+							src={require('../../../Img/share.svg').default}
+							className="shareIcon"
+						/>
+						<div className="shareVal">{challenge.shareCount}</div>
+					</div>
+				</section>
+			</div>
+		</EleContainer>
+	);
 };
 
 export default ContentElement;
