dee9bf5b1cfc76662e2c51aa209f9194dbfe47d9
hunhunseunghun <hunhunseunghun@gmail.com>
2021-07-02 12:11:34 +0900

    07021211

--- src/page/MyWork/tables/ParticipateTable.jsx
+++ src/page/MyWork/tables/ParticipateTable.jsx
@@ -1,73 +1,73 @@
 import React, { useState, useEffect } from 'react';
-import Pagination2 from '../../../Component/Pagination/Pagination2';
-import { paginate } from '../../../Component/Pagination/paginate';
+import Pagination2 from '../../../component/Pagination/Pagination2';
+import { paginate } from '../../../component/Pagination/paginate';
 import { ParticipateTableContainer } from './ParticipateTableStyled';
 function ParticipateTable({ datas }) {
-	let [quests, setQuests] = useState({
-		data: datas,
-		pageSize: 3,
-		currentPage: 1,
-	});
-	// console.log('datas: ', datas);
-	const { data, pageSize, currentPage } = quests;
-	const pagedQuests = paginate(data, currentPage, pageSize);
-	const { length: count } = quests.data;
-	console.log('datas: ', datas);
-	let handlePageChange = (page) => {
-		setQuests({ ...quests, currentPage: page });
-	};
-	useEffect(() => {
-		console.log('datas:');
-		console.log(datas);
-		setQuests({ ...quests, data: datas });
-	}, [datas]);
-	return (
-		<ParticipateTableContainer>
-			<table>
-				<thead>
-					<tr>
-						<th key={'image'}>이미지</th>
-						<th key={'category'}>카테고리</th>
-						<th key={'name'}>프로젝트명</th>
-						<th key={'status'}>상태</th>
-						<th key={'presentation'}>제출자료</th>
-						<th key={'feedback'}>피드백</th>
-						<th key={'requestDate'}>신청일</th>
-						<th key={'dueDate'}>마감일</th>
-					</tr>
-				</thead>
-				<tbody>
-					{pagedQuests
-						? pagedQuests.map((data) => {
-								return (
-									<tr>
-										<td>
-											{data.img !== '' ? (
-												<img src={data.img}></img>
-											) : (
-												'no image'
-											)}
-										</td>
-										<td>{data.category}</td>
-										<td>{data.name}</td>
-										<td>{data.status}</td>
-										<td>{data.presentation}</td>
-										<td>{data.feedback ? <button>피드백</button> : ''}</td>
-										<td>{data.requestDate}</td>
-										<td>{data.dueDate}</td>
-									</tr>
-								);
-						  })
-						: ''}
-				</tbody>
-			</table>
-			<div className="pagination">
-				<Pagination2
-					itemsCount={count}
-					handlePageChange={handlePageChange}
-				></Pagination2>
-			</div>
-		</ParticipateTableContainer>
-	);
+  let [quests, setQuests] = useState({
+    data: datas,
+    pageSize: 3,
+    currentPage: 1,
+  });
+  // console.log('datas: ', datas);
+  const { data, pageSize, currentPage } = quests;
+  const pagedQuests = paginate(data, currentPage, pageSize);
+  const { length: count } = quests.data;
+  console.log('datas: ', datas);
+  let handlePageChange = page => {
+    setQuests({ ...quests, currentPage: page });
+  };
+  useEffect(() => {
+    console.log('datas:');
+    console.log(datas);
+    setQuests({ ...quests, data: datas });
+  }, [datas]);
+  return (
+    <ParticipateTableContainer>
+      <table>
+        <thead>
+          <tr>
+            <th key={'image'}>이미지</th>
+            <th key={'category'}>카테고리</th>
+            <th key={'name'}>프로젝트명</th>
+            <th key={'status'}>상태</th>
+            <th key={'presentation'}>제출자료</th>
+            <th key={'feedback'}>피드백</th>
+            <th key={'requestDate'}>신청일</th>
+            <th key={'dueDate'}>마감일</th>
+          </tr>
+        </thead>
+        <tbody>
+          {pagedQuests
+            ? pagedQuests.map(data => {
+                return (
+                  <tr>
+                    <td>
+                      {data.img !== '' ? (
+                        <img src={data.img}></img>
+                      ) : (
+                        'no image'
+                      )}
+                    </td>
+                    <td>{data.category}</td>
+                    <td>{data.name}</td>
+                    <td>{data.status}</td>
+                    <td>{data.presentation}</td>
+                    <td>{data.feedback ? <button>피드백</button> : ''}</td>
+                    <td>{data.requestDate}</td>
+                    <td>{data.dueDate}</td>
+                  </tr>
+                );
+              })
+            : ''}
+        </tbody>
+      </table>
+      <div className="pagination">
+        <Pagination2
+          itemsCount={count}
+          handlePageChange={handlePageChange}
+        ></Pagination2>
+      </div>
+    </ParticipateTableContainer>
+  );
 }
 export default ParticipateTable;
--- src/page/WorkDetail/tables/Challenge.jsx
+++ src/page/WorkDetail/tables/Challenge.jsx
@@ -1,115 +1,115 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import Pagination2 from '../../../Component/Pagination/Pagination2';
-import { paginate } from '../../../Component/Pagination/paginate';
+import Pagination2 from '../../../component/Pagination/Pagination2';
+import { paginate } from '../../../component/Pagination/paginate';
 import { ChallengeTableContainer } from './ChallengeStyled';
 function ChallengeTable({ datas }) {
-	let [subjects, setSubjects] = useState({
-		data: datas,
-		pageSize: 3,
-		currentPage: 1,
-	});
-	// console.log('datas: ', datas);
-	const { data, pageSize, currentPage } = subjects;
-	const pagedQuests = paginate(data, currentPage, pageSize);
-	const { length: count } = subjects.data;
-	console.log('datas: ', datas);
-	let handlePageChange = (page) => {
-		setSubjects({ ...subjects, currentPage: page });
-	};
-	useEffect(() => {
-		console.log('datas:');
-		console.log(datas);
-		setSubjects({ ...subjects, data: datas });
-	}, [datas]);
-	return (
-		<ChallengeTableContainer>
-			<table>
-				<thead>
-					<th key="challengeTime">챌린지 일시</th>
-					<th key="ID">ID</th>
-					<th key="name">성명</th>
-					<th key="mobileNum">휴대폰 번호</th>
-					<th key="email">이메일</th>
-				</thead>
-				<tbody>
-					{pagedQuests
-						? pagedQuests.map((data) => {
-								return (
-									<>
-										<tr>
-											<td>
-												<img src={data.img}></img>
-											</td>
-											<td>{data.category}</td>
-											<td>
-												<Link
-													to={{
-														pathname: '/workdetail',
-														state: {
-															projectId: data.id,
-															isContriClicked: false,
-															isInspectClicked: false,
-														},
-													}}
-												>
-													{data.name}
-												</Link>
-											</td>
-											<td>
-												<Link
-													to={{
-														pathname: '/workdetail',
-														state: {
-															projectId: data.id,
-															isContriClicked: true,
-															isInspectClicked: false,
-														},
-													}}
-												>
-													{data.contriNum}
-												</Link>
-												{data.isNewContri ? (
-													<span className="newAlert">new</span>
-												) : (
-													''
-												)}
-											</td>
-											<td>
-												<Link
-													to={{
-														pathname: '/workdetail',
-														state: {
-															projectId: data.id,
-															isContriClicked: true,
-															isInspectClicked: false,
-														},
-													}}
-												>
-													{data.inspect}
-												</Link>
-												{data.isNewInspect ? (
-													<span className="newAlert">new</span>
-												) : (
-													''
-												)}
-											</td>
-											<td>{data.dueDate}</td>
-										</tr>
-										{/* <hr className="row-line"></hr> */}
-									</>
-								);
-						  })
-						: ''}
-				</tbody>
-			</table>
-			<div className="pagination">
-				<Pagination2
-					itemsCount={count}
-					handlePageChange={handlePageChange}
-				></Pagination2>
-			</div>
-		</ChallengeTableContainer>
-	);
+  let [subjects, setSubjects] = useState({
+    data: datas,
+    pageSize: 3,
+    currentPage: 1,
+  });
+  // console.log('datas: ', datas);
+  const { data, pageSize, currentPage } = subjects;
+  const pagedQuests = paginate(data, currentPage, pageSize);
+  const { length: count } = subjects.data;
+  console.log('datas: ', datas);
+  let handlePageChange = page => {
+    setSubjects({ ...subjects, currentPage: page });
+  };
+  useEffect(() => {
+    console.log('datas:');
+    console.log(datas);
+    setSubjects({ ...subjects, data: datas });
+  }, [datas]);
+  return (
+    <ChallengeTableContainer>
+      <table>
+        <thead>
+          <th key="challengeTime">챌린지 일시</th>
+          <th key="ID">ID</th>
+          <th key="name">성명</th>
+          <th key="mobileNum">휴대폰 번호</th>
+          <th key="email">이메일</th>
+        </thead>
+        <tbody>
+          {pagedQuests
+            ? pagedQuests.map(data => {
+                return (
+                  <>
+                    <tr>
+                      <td>
+                        <img src={data.img}></img>
+                      </td>
+                      <td>{data.category}</td>
+                      <td>
+                        <Link
+                          to={{
+                            pathname: '/workdetail',
+                            state: {
+                              projectId: data.id,
+                              isContriClicked: false,
+                              isInspectClicked: false,
+                            },
+                          }}
+                        >
+                          {data.name}
+                        </Link>
+                      </td>
+                      <td>
+                        <Link
+                          to={{
+                            pathname: '/workdetail',
+                            state: {
+                              projectId: data.id,
+                              isContriClicked: true,
+                              isInspectClicked: false,
+                            },
+                          }}
+                        >
+                          {data.contriNum}
+                        </Link>
+                        {data.isNewContri ? (
+                          <span className="newAlert">new</span>
+                        ) : (
+                          ''
+                        )}
+                      </td>
+                      <td>
+                        <Link
+                          to={{
+                            pathname: '/workdetail',
+                            state: {
+                              projectId: data.id,
+                              isContriClicked: true,
+                              isInspectClicked: false,
+                            },
+                          }}
+                        >
+                          {data.inspect}
+                        </Link>
+                        {data.isNewInspect ? (
+                          <span className="newAlert">new</span>
+                        ) : (
+                          ''
+                        )}
+                      </td>
+                      <td>{data.dueDate}</td>
+                    </tr>
+                    {/* <hr className="row-line"></hr> */}
+                  </>
+                );
+              })
+            : ''}
+        </tbody>
+      </table>
+      <div className="pagination">
+        <Pagination2
+          itemsCount={count}
+          handlePageChange={handlePageChange}
+        ></Pagination2>
+      </div>
+    </ChallengeTableContainer>
+  );
 }
 export default ChallengeTable;
