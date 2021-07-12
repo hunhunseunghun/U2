8f25fc945dd1be8a6cfa14f4e827f1e80efa0a4e
hunhunseunghun <hunhunseunghun@gmail.com>
2021-07-12 16:31:39 +0900

    project details

--- src/page/ProjectDetail/PrjInfo/ProjectInfoStyled.jsx
+++ src/page/ProjectDetail/PrjInfo/ProjectInfoStyled.jsx
@@ -1,9 +1,38 @@
 import styled from 'styled-components';
 export const PrjInfoContainer = styled.div`
-	background-color: yellow;
-	border: 1px solid;
-	th,
-	tr {
-		border: 1px solid;
-	}
+  width: 100%;
+
+  table {
+    width: 100%;
+    text-align: center;
+    border-top: 1px solid #afafaf;
+    border-bottom: 1px solid #afafaf;
+    padding: 0;
+    border-collapse: collapse;
+
+    th {
+      background-color: #f0f0f0;
+    }
+    th,
+    td {
+      padding: 5px;
+      border-bottom: 1px solid #dddddd;
+      border-right: 1px solid #dddddd;
+      font-weight: 400;
+    }
+
+    tr td:last-child {
+      border-right: 0;
+    }
+
+    tbody tr:last-child {
+      border-bottom: 1px solid #afafaf;
+      th,
+      td {
+        border-bottom: 0;
+      }
+    }
+  }
 `;
+
+// border-top: 1px solid #f0f0f0;
--- src/page/ProjectDetail/ProjectDetailStyled.jsx
+++ src/page/ProjectDetail/ProjectDetailStyled.jsx
@@ -1,8 +1,26 @@
 import styled from 'styled-components';
 
 export const ProjectDetailContainer = styled.div`
-	/* background-color: grey; */
-	.prj-control > button {
-		border: solid 1px;
-	}
+  /* background-color: grey; */
+  padding: 40px 45px 100px;
+  font-weight: 500;
+
+  .prj_title_area {
+    display: flex;
+    justify-content: space-between;
+    border-top: 2px solid #2d2123;
+    color: #010101;
+    font-size: 21px;
+    padding: 10px 15px;
+
+    .prj_term {
+      font-size: 13px;
+      color: #7f7f7f;
+      font-weight: 400;
+    }
+  }
+
+  .prj-control > button {
+    border: solid 1px;
+  }
 `;
