70cbc6c781070af28763b0ba36862697eb6e7c66
이찬휘 <cokids@u2.life>
2021-07-21 15:19:14 +0900

    URL.js 환경변수(.env) 값으로 변경

--- src/const/URL.js
+++ src/const/URL.js
@@ -1,11 +1,16 @@
 //상용
-const HOST_URL = 'https://u2.life';
-const API_URL = 'https://api.u2.life/api';
-const IMG_URL = 'https://admin.u2.life/upload';
+//const HOST_URL = 'https://u2.life';
+//const API_URL = 'https://api.u2.life/api';
+//const IMG_URL = 'https://admin.u2.life/upload';
 
 // //개발
 // const HOST_URL = 'https://web-test.u2.life';
 // const API_URL = 'https://api-test.u2.life/api';
 // const IMG_URL = 'https://admin-test.u2.life/upload';
+
+//환경변수
+const HOST_URL = process.env.REACT_APP_HOST_URL;
+const API_URL = process.env.REACT_APP_API_URL;
+const IMG_URL = process.env.REACT_APP_IMG_URL;
 
 export { HOST_URL, API_URL, IMG_URL };
