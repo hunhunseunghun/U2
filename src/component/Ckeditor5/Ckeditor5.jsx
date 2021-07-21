4156c96f6178c80a931b44c1bcbbaef67dfe2593
hunhunseunghun <hunhunseunghun@gmail.com>
2021-07-21 15:27:43 +0900

    handleckeditorvalue

--- src/component/Ckeditor5/Ckeditor5.jsx
+++ src/component/Ckeditor5/Ckeditor5.jsx
@@ -30,12 +30,14 @@ import TableCellProperties from '@ckeditor/ckeditor5-table/src/tablecellproperti
 import Base64UploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter';
 import { Container } from './Ckeditor5Styled.jsx';
 
-const Ckeditor = ({ handleChangeValue }) => {
+const Ckeditor = ({ handleCkeditorValue }) => {
   return (
     <Container className="ckeditor_wrap">
       <CKEditor
         className="ckeditor5"
-        onChange={handleChangeValue}
+        onChange={e => {
+          handleCkeditorValue(e);
+        }}
         onInit={editor => {
           editor.ui
             .getEditableElement()
