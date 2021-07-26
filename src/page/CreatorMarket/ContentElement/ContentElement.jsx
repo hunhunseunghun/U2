8a78ef2f864f5d5f75d3a772c9dd68e67c0dee4d
leehojun0314 <dlghwns0314@naver.com>
2021-07-27 02:51:45 +0900

    'badge'

--- src/page/CreatorMarket/ContentElement/ContentElement.jsx
+++ src/page/CreatorMarket/ContentElement/ContentElement.jsx
@@ -1,128 +1,148 @@
 import React, { useState, useEffect } from 'react';
 import { FiCheck, FiX } from 'react-icons/fi';
+import { BsStarFill } from 'react-icons/bs';
 import { EleContainer } from './ContentElementStyled.jsx';
 import blankImg from '../../../Img/no_image.png';
 import { calcRemainDays } from '../../../library/timeSetting.js';
 import { getSingleFileFromBlob } from '../../../library/azureBlob.js';
-const ContentElement = props => {
-  const [myRegistration, setMyRegistration] = useState(false);
-  const [mySubmit, setMySubmit] = useState(false);
-  const challenge = props.challenge;
-  const [src, setSrc] = useState('');
-  console.log('challenge inside contentelement: ', challenge);
-  useEffect(async () => {
-    if (challenge.logo) {
-      setSrc(await getSingleFileFromBlob(challenge.logo));
-    } else {
-      setSrc(blankImg);
-    }
-  });
-  return (
-    <EleContainer
-      onClick={() => {
-        props.history.push({
-          pathname: `/prjdetail/${challenge.challengeIdx}`,
-        });
-      }}
-    >
-      <div className="challenge_img_area">
-        <img src={src} alt={src} className="challenge_img" />
-        {/* {tItem.bannerImage === null && (
+import { useSelector } from 'react-redux';
+const ContentElement = (props) => {
+	const userInfo = useSelector((state) => state.userInfo);
+	const [myRegistration, setMyRegistration] = useState(false);
+	const [mySubmit, setMySubmit] = useState(false);
+	const challenge = props.challenge;
+	const [src, setSrc] = useState('');
+	console.log('challenge inside contentelement: ', challenge);
+	console.log('props: ', props);
+	useEffect(() => {
+		if (challenge.logo) {
+			setSrc(getSingleFileFromBlob(challenge.logo));
+		} else {
+			setSrc(blankImg);
+		}
+	});
+	return (
+		<EleContainer
+			onClick={() => {
+				props.history.push({
+					pathname: `/prjdetail/${challenge.challengeIdx}`,
+				});
+			}}
+		>
+			<div
+				className="challenge_img_area"
+				style={{ backgroundImage: `url(src)` }}
+			>
+				<img src={src} alt={src} className="challenge_img" />
+				{/* {tItem.bannerImage === null && (
           <div className={'no_img'}>
             <img src={'/img/no_image.png'} />
             <span className={'not_contents'}>Image not found</span>
           </div>
         )} */}
-      </div>
-      <div className="challenge_contents">
-        <h1 className="challenge_title">{challenge.title}</h1>
-        <div className="challenge_sub">{challenge.subtitle}</div>
-      </div>
+			</div>
+			<div>
+				{' '}
+				{props.badgeData.applies &&
+					props.badgeData.applies.has(30) && // should be userInfo.memberIdx
+					'내가 지원'}
+				{props.badgeData.submits &&
+					props.badgeData.submits.has(30) &&
+					'내가 제출'}
+				{props.badgeData.wishes && props.badgeData.wishes.has(30) && (
+					<BsStarFill />
+				)}
+				{challenge.memberIdx === userInfo.memberIdx && '내가 등록'}
+			</div>
+			<div className="challenge_contents">
+				<h1 className="challenge_title">{challenge.title}</h1>
+				<div className="challenge_sub">{challenge.subtitle}</div>
+			</div>
 
-      <div className="challenge_bottom">
-        <div className="challenge_progress_area">
-          <div className="challenge_progress_text">
-            {/* <button className="paticipantBtn">+</button> */}
-            <div>챌린지 참여자</div> <div>{`${challenge.challengerCount}`}</div>
-          </div>
-          {/* 
+			<div className="challenge_bottom">
+				<div className="challenge_progress_area">
+					<div className="challenge_progress_text">
+						{/* <button className="paticipantBtn">+</button> */}
+						<div>챌린지 참여자</div> <div>{`${challenge.challengerCount}`}</div>
+					</div>
+					{/* 
 					<LinearProgress
 						className="challenge_progressBar"
 						variant="determinate"
 						color="secondary"
 						value={challenge.challengerCount}
 					/> */}
-        </div>
-        <section className="challenge_info_top">
-          <div className="meetArea">
-            {/* <input type="checkbox" name="chk_info" value="HTML" /> */}
-            <div className="meet_onoff">
-              {challenge.meetCode === 0 ? <FiX /> : <FiCheck />}
-            </div>
-            <div className="meetText">OFF 미팅</div>
-          </div>
-          <div className="budgetArea">
-            <div className="budget">
-              {challenge.reward ? challenge.reward : '---'}
-            </div>
-            <div className="bugetText">예산금액</div>
-          </div>
-          <div className="remainDateArea">
-            <div className="remainDate">
-              {challenge.missions.length > 0 &&
-                calcRemainDays(challenge.missions[0].dateFin)}
-              일
-            </div>
-            <div className="remainText">남은 일자</div>
-          </div>
-        </section>
-        <section className="challenge_info_top_mobile">
-          <div className="meetArea">
-            {/* <input type="checkbox" name="chk_info" value="HTML" /> */}
+				</div>
+				<section className="challenge_info_top">
+					<div className="meetArea">
+						{/* <input type="checkbox" name="chk_info" value="HTML" /> */}
+						<div className="meet_onoff">
+							{challenge.meetCode === 0 ? <FiX /> : <FiCheck />}
+						</div>
+						<div className="meetText">OFF 미팅</div>
+					</div>
+					<div className="budgetArea">
+						<div className="budget">
+							{challenge.reward ? challenge.reward : '---'}
+						</div>
+						<div className="bugetText">예산금액</div>
+					</div>
+					<div className="remainDateArea">
+						<div className="remainDate">
+							{challenge.missions.length > 0 &&
+								calcRemainDays(challenge.missions[0].dateFin)}
+							일
+						</div>
+						<div className="remainText">남은 일자</div>
+					</div>
+				</section>
+				<section className="challenge_info_top_mobile">
+					<div className="meetArea">
+						{/* <input type="checkbox" name="chk_info" value="HTML" /> */}
 
-            <div className="meetText">OFF 미팅</div>
-            <div className="meet_onoff">
-              {challenge.meetCode === 0 ? <FiX /> : <FiCheck />}
-            </div>
-          </div>
-          <div className="budgetArea">
-            <div className="bugetText">예산금액</div>
-            <div className="budget">
-              {challenge.reward
-                ? challenge.reward
-                : '--vadasdfadfavsdfasdvadfasdf-'}
-            </div>
-          </div>
-          <div className="remainDateArea">
-            <div className="remainText">남은 일자</div>
-            <div className="remainDate">
-              {challenge.missions.length > 0 &&
-                calcRemainDays(challenge.missions[0].dateFin)}
-              일
-            </div>
-          </div>
-        </section>
+						<div className="meetText">OFF 미팅</div>
+						<div className="meet_onoff">
+							{challenge.meetCode === 0 ? <FiX /> : <FiCheck />}
+						</div>
+					</div>
+					<div className="budgetArea">
+						<div className="bugetText">예산금액</div>
+						<div className="budget">
+							{challenge.reward
+								? challenge.reward
+								: '--vadasdfadfavsdfasdvadfasdf-'}
+						</div>
+					</div>
+					<div className="remainDateArea">
+						<div className="remainText">남은 일자</div>
+						<div className="remainDate">
+							{challenge.missions.length > 0 &&
+								calcRemainDays(challenge.missions[0].dateFin)}
+							일
+						</div>
+					</div>
+				</section>
 
-        <section className="challenge_info_bot">
-          <div className="challenge_cmtval_area">
-            <img
-              src={require('../../../Img/comment.svg').default}
-              className="cmntIcon"
-            />
-            <div className="cmntVal">{challenge.commentCount}</div>
-          </div>
+				<section className="challenge_info_bot">
+					<div className="challenge_cmtval_area">
+						<img
+							src={require('../../../Img/comment.svg').default}
+							className="cmntIcon"
+						/>
+						<div className="cmntVal">{challenge.commentCount}</div>
+					</div>
 
-          <div className="challenge_shareval_area">
-            <img
-              src={require('../../../Img/share.svg').default}
-              className="shareIcon"
-            />
-            <div className="shareVal">{challenge.shareCount}</div>
-          </div>
-        </section>
-      </div>
-    </EleContainer>
-  );
+					<div className="challenge_shareval_area">
+						<img
+							src={require('../../../Img/share.svg').default}
+							className="shareIcon"
+						/>
+						<div className="shareVal">{challenge.shareCount}</div>
+					</div>
+				</section>
+			</div>
+		</EleContainer>
+	);
 };
 
 export default ContentElement;
